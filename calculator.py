9d3ef9c5412a30070d6186dfdecb14460d9c42c3
kaveen <77354559+kaveen27@users.noreply.github.com>
2021-04-12 14:49:52 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+#def add(a,b):
+ #   print(a+b)
+while True:   
+
+
+    a=int(input("enter the no :"))
+    b=int(input("enter the no :"))
+    c=input("enter the values for calculation ,+,-,*,/: ")
+    if c=="+":
+        print(a+b)
+    elif c=="-":
+                print(a-b)
+    elif c=="/":
+                print(a/b)
+    elif c=="*":
+                print(a*b)
+    else:
+        print("enter the proper value")
+#add(a,b)
